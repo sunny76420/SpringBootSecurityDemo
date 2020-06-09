678e597630c3d455b1a1018aa50a410f5533bc2e
Ashish Mathur <ashishmathur@DLLA38P3561.qait.com>
2020-06-09 19:23:13 +0530

    Fixed permission issue

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.example.demo.security.ApplicationUserRole.*;
+
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,12 +27,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
-//		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-		.antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name()) //Role based Authentication.
-		.antMatchers(HttpMethod.DELETE, "management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-		.antMatchers(HttpMethod.POST, "management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-		.antMatchers(HttpMethod.PUT, "management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-		.antMatchers(HttpMethod.GET, "management/api/**").hasAnyRole(ApplicationUserRole.ADMIN.name(), ApplicationUserRole.INSTRUCTOR.name())
+		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+		.antMatchers("/api/**").hasRole(STUDENT.name()) //Role based Authentication.
+		.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+		.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+		.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+		.antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), INSTRUCTOR.name())
 		.anyRequest().authenticated().and().httpBasic();
 	}
 
@@ -40,17 +42,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		UserDetails admin = User.builder().username("ashish").password(passwordEncoder.encode("123456"))
 //				.roles(ApplicationUserRole.ADMIN.name())
-				.authorities(ApplicationUserRole.ADMIN.getGrantedAutorities())
+				.authorities(ADMIN.getGrantedAutorities())
 				.build();
 		
 		UserDetails instructor = User.builder().username("savita").password(passwordEncoder.encode("123456"))
 //				.roles(ApplicationUserRole.INSTRUCTOR.name())
-				.authorities(ApplicationUserRole.INSTRUCTOR.getGrantedAutorities())
+				.authorities(INSTRUCTOR.getGrantedAutorities())
 				.build();
 		
 		UserDetails student = User.builder().username("anna").password(passwordEncoder.encode("password"))
 //				.roles(ApplicationUserRole.STUDENT.name())
-				.authorities(ApplicationUserRole.STUDENT.getGrantedAutorities())
+				.authorities(STUDENT.getGrantedAutorities())
 				.build();
 
 		return new InMemoryUserDetailsManager(admin, student, instructor);
--- src/main/java/com/example/demo/security/ApplicationUserRole.java
+++ src/main/java/com/example/demo/security/ApplicationUserRole.java
@@ -26,7 +26,7 @@ public enum ApplicationUserRole {
 		Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
 		.map(permission -> new SimpleGrantedAuthority(permission.getPermission())).collect(Collectors.toSet());
 		
-		permissions.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+		permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
 		
 		return permissions;
 	}
