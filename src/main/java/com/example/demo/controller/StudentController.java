3c26c10e12ba889a1905b0c3c34fd22a7eb96adf
Ashish Mathur <ashishmathur@DLLA38P3561.qait.com>
2020-06-09 18:01:24 +0530

    updated permission based security changes

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/example/demo/security/ApplicationUserRole.java
+++ src/main/java/com/example/demo/security/ApplicationUserRole.java
@@ -1,6 +1,9 @@
 package com.example.demo.security;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 import com.google.common.collect.Sets;
 import static com.example.demo.security.ApplicationUserPermission.*;
 
@@ -16,6 +19,15 @@ public enum ApplicationUserRole {
 	}
 
 	public Set<ApplicationUserPermission> getPermissions() {
+		return permissions;
+	}
+	
+	public Set<SimpleGrantedAuthority> getGrantedAutorities(){
+		Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+		.map(permission -> new SimpleGrantedAuthority(permission.getPermission())).collect(Collectors.toSet());
+		
+		permissions.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+		
 		return permissions;
 	}
 
