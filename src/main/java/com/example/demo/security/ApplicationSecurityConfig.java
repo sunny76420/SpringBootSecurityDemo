b12e6f2187c428819d9ac320997759c9f3599bd3
Ashish Mathur <ashishmathur@DLLA38P3561.qait.com>
2020-06-16 12:38:59 +0530

    Enabled CSRF security

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.example.demo.security.ApplicationUserRole.*;
 
@@ -28,14 +29,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
+		http
+//		.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+		.authorizeRequests()
 		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
 		.antMatchers("/api/**").hasRole(STUDENT.name()) //Role based Authentication.
-//		.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-//		.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-//		.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-//		.antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), INSTRUCTOR.name())
-		.anyRequest().authenticated().and().httpBasic();
+		.anyRequest()
+		.authenticated()
+		.and()
+		.httpBasic();
 	}
 
 	@Override
@@ -43,17 +45,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected UserDetailsService userDetailsService() {
 		
 		UserDetails admin = User.builder().username("ashish").password(passwordEncoder.encode("123456"))
-//				.roles(ApplicationUserRole.ADMIN.name())
 				.authorities(ADMIN.getGrantedAutorities())
 				.build();
 		
 		UserDetails instructor = User.builder().username("savita").password(passwordEncoder.encode("123456"))
-//				.roles(ApplicationUserRole.INSTRUCTOR.name())
 				.authorities(INSTRUCTOR.getGrantedAutorities())
 				.build();
 		
 		UserDetails student = User.builder().username("anna").password(passwordEncoder.encode("password"))
-//				.roles(ApplicationUserRole.STUDENT.name())
 				.authorities(STUDENT.getGrantedAutorities())
 				.build();
 
