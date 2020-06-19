b5fdbff7a24e8bc6943f56f045ce74617731c3ac
Ashish Mathur <ashishmathur@DLLA38P3561.qait.com>
2020-06-19 10:26:06 +0530

    Updated form-login with login and logout

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,8 +11,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static com.example.demo.security.ApplicationUserRole.*;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
@@ -28,14 +30,26 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
+		http
+		.csrf().disable()
+		.authorizeRequests()
 		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
 		.antMatchers("/api/**").hasRole(STUDENT.name()) //Role based Authentication.
-//		.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-//		.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-//		.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-//		.antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), INSTRUCTOR.name())
-		.anyRequest().authenticated().and().httpBasic();
+		.anyRequest()
+		.authenticated().and()
+		.formLogin()
+		.loginPage("/login").permitAll()
+		.defaultSuccessUrl("/courses", true)
+		.and()
+		.rememberMe().tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
+		.key("securedkey")
+		.and()
+		.logout().logoutUrl("/logout")
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+		.clearAuthentication(true)
+		.invalidateHttpSession(true)
+		.deleteCookies("JSESSIONID", "remember-me")
+		.logoutSuccessUrl("/login");
 	}
 
 	@Override
@@ -43,17 +57,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected UserDetailsService userDetailsService() {
 		
 		UserDetails admin = User.builder().username("ashish").password(passwordEncoder.encode("123456"))
-//				.roles(ApplicationUserRole.ADMIN.name())
 				.authorities(ADMIN.getGrantedAutorities())
 				.build();
 		
 		UserDetails instructor = User.builder().username("savita").password(passwordEncoder.encode("123456"))
-//				.roles(ApplicationUserRole.INSTRUCTOR.name())
 				.authorities(INSTRUCTOR.getGrantedAutorities())
 				.build();
 		
 		UserDetails student = User.builder().username("anna").password(passwordEncoder.encode("password"))
-//				.roles(ApplicationUserRole.STUDENT.name())
 				.authorities(STUDENT.getGrantedAutorities())
 				.build();
 
